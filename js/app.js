0ef2039e9d71faf9e588d90f0bae0e87b76b0b45
Alyssa Balaoro <balaoroalyssa@gmail.com>
2021-12-11 13:53:48 +0800

    Initialize JS script

--- js/app.js
+++ js/app.js
@@ -4,27 +4,20 @@ document.querySelector('#generate-names').addEventListener('submit', loadNames);
 
 // Execute the function to query the API
 function loadNames(e) {
-     e.preventDefault();
+    e.preventDefault();
 
 
-     // Read the values from the form and create the variables
-     const origin = document.getElementById('country').value;
-     const genre = document.getElementById('genre').value;
-     const amount = document.getElementById('quantity').value;
+    // Read the values from the form and create the variables
+    const origin = document.getElementById('country').value;
+    const genre = document.getElementById('genre').value;
+    const amount = document.getElementById('quantity').value;
 
-     // Build the URL
-     let url = 'http://uinames.com/api/?';
-     // Read the origin and append to the url
-     if(origin !== ''){
-          url += `region=${origin}&`;
-     }
-     // Read the genre and append to the url
-     if(genre !== ''){
-          url += `gender=${genre}&`;
-     }    
-      // Read the amount and append to the url
-     if(amount !== ''){
-          url += `amount=${amount}&`;
-     }
-     
-}
+    // Build the URL
+    let url = 'https://randomuser.me/api/?';
+    //Read the origin and append to the url
+    if(origin !== ''){
+        url += `nat=${origin}&`;
+    }
+    
+     console.log(url);
+}  
